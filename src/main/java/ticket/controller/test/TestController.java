5bf6a2b23332976d7ea3a7d7095bbdcbf1c8cf86
SSOL <livingsnow011@gmail.com>
2022-01-21 17:30:24 +0900

    HS commit

--- src/main/java/ticket/controller/test/TestController.java
+++ src/main/java/ticket/controller/test/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ticket.dto.ShowInfoResponseDto;
 import ticket.service.ShowAndSeatService;
 
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,25 +21,40 @@ public class TestController {
     @GetMapping("/show/movie")
     public String movieInfo(Model model){
         List<ShowInfoResponseDto> shows = showAndSeatService.findAllShowInfo();
-        model.addAttribute("subject","영화 목록");
+
+        shows.removeIf(n -> (!n.getClassification().equals("movie")));
+
+        model.addAttribute("subject","MOVIE");
         model.addAttribute("shows",shows);
-        return "test/show";
+        model.addAttribute("size",shows.size());
+
+        return "show";
     }
 
     @GetMapping("/show/play")
     public String playInfo(Model model){
         List<ShowInfoResponseDto> shows = showAndSeatService.findAllShowInfo();
-        model.addAttribute("subject","연극 목록");
+
+        shows.removeIf(n -> (!n.getClassification().equals("play")));
+
+        model.addAttribute("subject","PLAY");
         model.addAttribute("shows",shows);
-        return "test/show";
+        model.addAttribute("size",shows.size());
+
+        return "show";
     }
 
     @GetMapping("/show/concert")
     public String concertInfo(Model model){
         List<ShowInfoResponseDto> shows = showAndSeatService.findAllShowInfo();
-        model.addAttribute("subject","콘서트 목록");
+
+        shows.removeIf(n -> (!n.getClassification().equals("concert")));
+
+        model.addAttribute("subject","CONCERT");
         model.addAttribute("shows",shows);
-        return "test/show";
+        model.addAttribute("size",shows.size());
+
+        return "show";
     }
 
 }
