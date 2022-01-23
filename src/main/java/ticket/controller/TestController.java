abcb13ddcfd8b87bec95da252f38154baee1f0b5
SeSAC-Cloud-CTW <slowlight50@gmail.com>
2022-01-23 20:53:37 +0900

    로그인 상태에 따라 헤더의 버튼 표시가 바뀌도록 수정 공통적으로 사용되는 script들을 분리 예매 기능 구현

--- src/main/java/ticket/controller/TestController.java
+++ src/main/java/ticket/controller/TestController.java
@@ -1,4 +1,4 @@
-package ticket.controller.test;
+package ticket.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -28,8 +28,20 @@ public class TestController {
     private final UserService userService;
     private final LogService logService;
 
+    @GetMapping("/")
+    public String index(@CookieValue(name = "userId", required = false) Cookie cookie, Model model){
+        if (!ObjectUtils.isEmpty(cookie)) {
+            addUserNameToModel(model, Long.parseLong(cookie.getValue()));
+        }
+
+        return "index";
+    }
+
     @GetMapping("/show/movie")
-    public String movieInfo(Model model) {
+    public String movieInfo(@CookieValue(name = "userId", required = false) Cookie cookie, Model model) {
+        if (!ObjectUtils.isEmpty(cookie)) {
+            addUserNameToModel(model, Long.parseLong(cookie.getValue()));
+        }
         List<ShowInfoResponseDto> shows = showAndSeatService.findAllShowInfo();
 
         shows.removeIf(n -> (!n.getClassification().equals("movie")));
@@ -43,7 +55,10 @@ public class TestController {
     }
 
     @GetMapping("/show/play")
-    public String playInfo(Model model) {
+    public String playInfo(@CookieValue(name = "userId", required = false) Cookie cookie, Model model) {
+        if (!ObjectUtils.isEmpty(cookie)) {
+            addUserNameToModel(model, Long.parseLong(cookie.getValue()));
+        }
         List<ShowInfoResponseDto> shows = showAndSeatService.findAllShowInfo();
 
         shows.removeIf(n -> (!n.getClassification().equals("play")));
@@ -57,7 +72,10 @@ public class TestController {
     }
 
     @GetMapping("/show/concert")
-    public String concertInfo(Model model) {
+    public String concertInfo(@CookieValue(name = "userId", required = false) Cookie cookie, Model model) {
+        if (!ObjectUtils.isEmpty(cookie)) {
+            addUserNameToModel(model, Long.parseLong(cookie.getValue()));
+        }
         List<ShowInfoResponseDto> shows = showAndSeatService.findAllShowInfo();
 
         shows.removeIf(n -> (!n.getClassification().equals("concert")));
@@ -71,7 +89,11 @@ public class TestController {
     }
 
     @GetMapping("/ticketing")
-    public String ticketInfo(@RequestParam String showId, Model model) {
+    public String ticketInfo(@RequestParam String showId, @CookieValue(name = "userId", required = false) Cookie cookie, Model model) {
+        if (!ObjectUtils.isEmpty(cookie)) {
+            addUserNameToModel(model, Long.parseLong(cookie.getValue()));
+        }
+
         long id = Long.parseLong(showId);
         ShowInfoResponseDto show = showAndSeatService.findShowInfoById(id);
         List<SeatResponseDto> seats = showAndSeatService.findSeatByShowInfoId(id);
@@ -91,14 +113,34 @@ public class TestController {
     }
 
     @GetMapping("/show/register")
-    public String registerShow(@CookieValue(name = "userId", required = false) Cookie cookie) throws Exception {
+    public String registerShow(@CookieValue(name = "userId", required = false) Cookie cookie, Model model) throws Exception {
         if (ObjectUtils.isEmpty(cookie)) {
             throw new NullPointerException();
+        } else {
+            addUserNameToModel(model, Long.parseLong(cookie.getValue()));
         }
+
 
         UserDTO user = userService.getUsersByUserId(Long.parseLong(cookie.getValue()));
         if (user.getRole().equalsIgnoreCase("admin")) {
             return "registerShow";
+        } else {
+            throw new Exception("Forbidden");
+        }
+    }
+
+    @GetMapping("/admin/users")
+    public String administrateUsers(@CookieValue(name = "userId", required = false) Cookie cookie, Model model) throws Exception {
+        if (ObjectUtils.isEmpty(cookie)) {
+            throw new NullPointerException();
+        } else {
+            addUserNameToModel(model, Long.parseLong(cookie.getValue()));
+        }
+
+
+        UserDTO user = userService.getUsersByUserId(Long.parseLong(cookie.getValue()));
+        if (user.getRole().equalsIgnoreCase("admin")) {
+            return "/admin/users";
         } else {
             throw new Exception("Forbidden");
         }
@@ -109,6 +151,7 @@ public class TestController {
         if (ObjectUtils.isEmpty(cookie)) {
             throw new NullPointerException();
         }
+        addUserNameToModel(model, Long.parseLong(cookie.getValue()));
 
         UserDTO userDTO = userService.getUsersByUserId(Long.parseLong(cookie.getValue()));
         ResponseUser user = new ModelMapper().map(userDTO, ResponseUser.class);
@@ -119,5 +162,10 @@ public class TestController {
         model.addAttribute("logs", logs);
 
         return "myPage";
+    }
+
+    public void addUserNameToModel(Model model, Long id) {
+        String userName = userService.getUsersByUserId(id).getName();
+        model.addAttribute("userName", userName);
     }
 }
