f62e75076b525fa1a4636317584e3e7dd1036cda
SSOL <livingsnow011@gmail.com>
2022-03-07 23:00:20 +0900

    create jar

--- src/main/java/ticket/config/WebMvcConfiguration.java
+++ src/main/java/ticket/config/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package ticket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +23,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         commonsMultipartResolver.setMaxUploadSizePerFile(20 * 1024 * 1024); // 20M
         return commonsMultipartResolver;
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    };
 
 }
